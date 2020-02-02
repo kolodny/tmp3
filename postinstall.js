8f9cae11e8e3744c76d5e4e3128ce85b1869a87a
Moshe Kolodny <kolodny@github.com>
2020-02-02 11:05:47 -0500

    foo4

--- postinstall.js
+++ postinstall.js
@@ -3,9 +3,11 @@ var path = require('path');
 const root = process.cwd();
 
 var link = root + '/node_modules/~/~';
+console.log(link);
 try {
   path.resolve(fs.realpathSync(link));
 } catch (e) {
+  console.log({root, link})
   fs.symlinkSync(root, link);
   process.exit(0);
 }
